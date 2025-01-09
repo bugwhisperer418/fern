7c4bce05ef0095ccb3a386f6bb88f567da478d0e
Andie Keller <andie@bugwhisperer.dev>
2025-01-09 16:20:24 +0800

    v0.0.7

---
+++ README.md
@@ -0,0 +1,30 @@
+# Fern
+
+`fern` is a swiss-army knife for your notetaking and personal knowledge management. Fern is a commandline tool to manage, curate, and search a vault of your personal notes. It offers an alternative to heavier GUI-based notetaking management applications. It was designed for systems that are resource constrained or for users that value the privacy and security of their data.
+
+Fern has support for daily Journals (think a Engineer's log), Templates, and Bookmarking Notes. It has a RegEx compatable full-text search of Notes to find records in your Vault.
+
+## Installing Fern
+1. Clone this repo.
+```sh
+git clone https://git.sr.ht/~bugwhisperer/fern
+```
+2. Change into this repo directory.
+```sh
+cd fern
+```
+4. Run the `install.sh` script
+```sh
+./install.sh
+```
+5. Setup a new Fern Vault.
+```sh
+fern vault create <path-to-vault-folder>
+```
+
+## Using Fern
+After installing fern, run `fern help` or, if you opt to install the manpage documentation, `man fern` for more guidance on using fern.
+
+## Found a bug? Feature idea?
+Please open any tickets for bugs found or feature requests here: [https://todo.sr.ht/~bugwhisperer/Fern-Issues](https://todo.sr.ht/~bugwhisperer/Fern-Issues).
+
---
+++ src/fern.sh
@@ -0,0 +1,384 @@
+#! /usr/bin/env bash
+#
+# Author: Andie Keller <andie@bugehisperer.dev>
+
+#{{{ Shell settings
+set -o errexit;		# abort on nonzero exitstatus
+set -o nounset;		# abort on unbound variable
+#}}}
+
+#{{{ Variables
+fVersion="0.0.7";
+readonly fVersion;
+
+if [[ ${FERN_VAULT:-"unset"} != "unset" ]]; then
+	fNotes="$FERN_VAULT/notes";
+	fJournals="$FERN_VAULT/journals";
+	fTemplates="$FERN_VAULT/templates";
+	fBookmarks="$FERN_VAULT/bookmarks";
+	dj="$fTemplates/daily-journal.md";
+	temp="$FERN_VAULT/.temp";
+	readonly fNotes fJournals fTemplates fBookmarks dj temp;
+fi
+#}}}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~         MAIN LOGIC         ~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+main() {
+	if help_wanted "$@"; then
+		usage_lite;
+	elif [ "${1}" = "help" ]; then
+		usage_indepth;
+	elif [ "${1}" = "vault" ]; then
+		process_vault "${@}";
+	else
+		if [[ ${FERN_VAULT:-"unset"} == "unset" ]] || [ ! -e "${FERN_VAULT}" ]; then
+			printf "Error: Fern Vault folder does not exist or the environment variable FERN_VAULT is not setup correctly.\nRun 'fern vault create <path>' to setup a new Fern Vault folder for management.\nIf you already went through the Fern Vault creation process, please verify that the FERN_VAULT environment variable is set correctly. You can verify this with 'echo \$FERN_VAULT'.\n" >&2;
+			exit 1;
+		fi
+
+		if [ ! -e "${dj}" ]; then
+			print_err "Error: The required 'daily-journal.md' template file does not exist! Please create one at '$dj' and then try again.";
+		fi
+		decipher_request "${@}";
+	fi
+	exit 0;
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~    HELPER FUNCTIONS      ~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# catches if no args are passed OR the first arg is a "common" help flag
+help_wanted() {
+	[ "$#" -eq 0 ] || [ "${1}" = "-h" ] || [ "${1}" = "--help" ] || [ "${1}" = "-?" ]
+}
+
+usage_lite() {
+	printf "fern - a knowledge management system [v%s]\nUsage:\tfern COMMAND [ACTION [ARGS...]]\n\nfern is a swiss-army knife for your notetaking and personal knowledge management. Fern is a helpful commandline tool to manage, curate, and search a vault of your personal notes.\n\nTo get started with a new Fern Vault, use 'fern vault create <path_to_vault>'.\nFor a brief list of all commands and their options, use 'fern help'.\nFor more in-depth documentation and advanced usage see the fern(1) manpage (\"man fern\") and/or https://sr.ht/~bugwhisperer/fern/\n" "$fVersion";
+}
+
+usage_indepth() {
+	printf "USAGE:\n  fern COMMAND [ACTION [ARGS...]]\n\nCOMMANDS:\n  help\t\t\t\t\t\t\tdisplay Fern help\n\n  vault create <path>\t\t\t\t\tsetup a new Fern Vault\n  vault stat\t\t\t\t\t\tget stats of Fern Vault\n\n  bookmark list\t\t\t\t\t\tlist out all Bookmarks\n  bookmark add <note_name>\t\t\t\tadd a note to Bookmarks\n  bookmark del <note_name>\t\t\t\tremove a note from Bookmarks\n\n  journal\t\t\t\t\t\topens today's Journal entry\n  journal open yesterday|today|tomorrow|<YYYY-MM-DD>\topen a Journal entry\n  journal find <pattern>\t\t\t\tsearch Journals for matching pattern\n\n  note add <note> [<template>]\t\t\t\tadd a new Note\n  note del <note>\t\t\t\t\tdelete a Note\n  note open <note>\t\t\t\t\topen a Note\n  note move <old_note> <new_note>\t\t\tmove/rename a Note\n  note find <pattern>\t\t\t\t\tsearch Notes for matching pattern\n\n  template list\t\t\t\t\t\tget list of Template files\n  template add <template>\t\t\t\tadd a new Template\n  template del <template>\t\t\t\tdelete a Template\n  template open <template>\t\t\t\topen a Template\n  template move <old_template> <new_template>\t\tmove/rename a Template\n\nFor more in-depth documentation and advanced usage see the fern(1) manpage (\"man fern\") and/or https://sr.ht/~bugwhisperer/fern/\n";
+}
+
+cmd_usage() {
+	case "$1" in
+	vault)
+		printf "Usage:\n  fern vault stat\n  fern vault create <path>\n";
+		;;
+	bookmark)
+		printf "Usage:\n  fern bookmark list\n  fern bookmark add|del <note_name>\n";
+		;;
+	journal)
+		printf "Usage:\n  fern journal\n  fern journal open yesterday|today|tomorrow|<YYYY-MM-DD>\n  fern journal find <search_pattern> [--verbose]\n";
+		;;
+	note)
+		printf "Usage:\n  fern note\n  fern note del|open <note_name>\n  fern note add <note_name> [<template_name>]\n  fern note find <search_pattern> [--verbose]\n  fern note move <old_note> <new_note>\n  fern note append <template_name> [<note_name>]\n";
+		;;
+	template)
+		printf "Usage:\n  fern template list\n  fern template add|del|open <template_name>\n  fern template move <old_template> <new_template>\n";
+		;;
+	esac
+	exit 0;
+}
+
+# Takes all user arguments "$@". Tries to find the right command >> action to execute.
+decipher_request() {
+	case "$1" in
+	vault)
+		process_vault "$@";
+		;;
+	bookmark)
+		process_bookmark "$@";
+		;;
+	journal)
+		process_journal "$@";
+		;;
+	note)
+		process_note "$@";
+		;;
+	template)
+		process_template "$@";
+		;;
+	*)
+		printf "Usage: Invalid command - '%s'. See \"fern help\" or the manpage (\"man fern\") for guidance.\n" "$1";
+		;;
+	esac
+}
+
+# $1 - action; $2 - Vault path;
+process_vault() {
+	if [ "$#" -lt 2 ]; then cmd_usage "$1"; fi
+	case "$2" in
+	create)
+		if [ "$#" -ne 3 ]; then cmd_usage "$1";
+		elif [ -e "$3" ]; then
+			print_err "Error: Folder already exists'$3'\n";
+		fi
+		# setup a new folder for management
+		mkdir --parents "$3";
+		mkdir --parents "$3/notes";
+		mkdir --parents "$3/journals";
+		mkdir --parents "$3/templates";
+		touch "$3/bookmarks";
+		touch "$3/templates/daily-journal.md";
+		printf "# {{DATE}}" | tee --append "$3/templates/daily-journal.md" >/dev/null;
+
+		printf "A Fern Vault has been setup for you at '%s'.\nYou're all ready to start managing your notes!\nBe sure to review 'fern help' for more information on the available commands and their arguments.\nA starter 'daily-journal' template file was created at '%s/daily-journal.md'.\nFeel free to add any starting contents that you require of your daily journal entries.\n" "$3" "$3/templates";
+		printf "\n~~ IMPORTANT! ~~\nIn order for fern to know where to find your Vault, add the following to your shell's RC File (ex. \$HOME/.bashrc or \$HOME/.zshrc):\n";
+		printf "export FERN_VAULT=\"%s\"\n" "$3";
+		;;
+	stat)
+		# Vault stats printed out
+		cntB=$(wc --lines < "$fBookmarks");
+		cntN=$(find "$fNotes" | wc --lines);
+		cntJ=$(find "$fJournals" | wc --lines);
+		cntT=$(find "$fTemplates" | wc --lines);
+		printf "Vault Location: $FERN_VAULT\n# Bookmarks:\t%d\n# Journals:\t%d\n# Notes:\t%d\n# Templates:\t%d\n" "$cntB" "$cntJ" "$cntN" "$cntT";
+		;;
+	*)
+		cmd_usage "$1";
+		;;
+	esac
+}
+
+# $1 - action; $2 - Note name;
+process_bookmark() {
+	if [ "$#" -lt 2 ]; then cmd_usage "$1"; fi
+	case "$2" in
+	list)
+		cat "$fBookmarks";
+		;;
+	add)
+		if [ "$#" -ne 3 ]; then cmd_usage "$1"; fi
+		target=$(file_checks "$3" "Usage: fern bookmarks add <note_name>");
+		if [ ! -e "$fNotes/$target" ]; then
+			print_err "Error: Note not found with the name '$target'.";
+		elif [ "$(awk "/$target/" < "$fBookmarks" | wc --lines)" -eq 0 ]; then
+			printf "%s" "$target" | tee --append "$fBookmarks" >/dev/null;
+			sort "$fBookmarks" -o "$fBookmarks";
+		fi
+		;;
+	del)
+		if [ "$#" -ne 3 ]; then cmd_usage "$1"; fi
+		target=$(file_checks "$3" "Usage: fern bookmarks del <note_name>");
+		mktemp -d -t "$temp";
+		trap '{ rm -f -- "$temp"; }' EXIT;
+		grep -xv "$target" "$fBookmarks" > "$temp" && mv "$temp" "$fBookmarks";
+		;;
+	*)
+		cmd_usage "$1";
+		;;
+	esac
+}
+
+# $1 - action; $2 - template name; $3 - template name;
+process_template() {
+	if [ "$#" -lt 2 ]; then cmd_usage "$1"; fi
+	case "$2" in
+	list)
+		ls "$fTemplates" -1;
+		;;
+	open)
+		if [ "$#" -lt 3 ]; then cmd_usage "$1"; fi
+		target=$(file_checks "$3" "Usage: fern template open <template_name>");
+		if [ ! -e "$fTemplates/$target" ]; then
+			print_err "Error: Template not found with the name '$target'.";
+		fi
+		$EDITOR "$fTemplates/$target";
+		;;
+	add)
+		if [ "$#" -lt 3 ]; then cmd_usage "$1"; fi
+		target=$(file_checks "$3" "Usage: fern template add <template_name>");
+		if [ -e "$target" ]; then
+			print_err "Error: Template already exists with that name '$target'.";
+		fi
+		$EDITOR "$fTemplates/$target";
+		;;
+	del)
+		if [ "$#" -lt 3 ]; then cmd_usage "$1"; fi
+		target=$(file_checks "$3" "Usage: fern template del <template_name>");
+		if [ ! -e "$fTemplates/$target" ]; then
+			print_err "Error: Template not found with the name '$target'.";
+		fi
+		rm -f "$fTemplates/$target";
+		;;
+	move)
+		if [ "$#" -lt 3 ]; then cmd_usage "$1"; fi
+		oldT=$(file_checks "$3" "Usage: fern template move <old_template> <new_template>");
+		if [ "$#" -lt 4 ]; then cmd_usage "$1"; fi
+		newT=$(file_checks "$4" "Usage: fern template move <old_template> <new_template>");
+		if [ ! -e "$fTemplates/$oldT" ]; then
+			print_err "Error: Template not found with the name '$oldT'.";
+		fi
+		mv "$fTemplates/$oldT" "$fTemplates/$newT";
+		;;
+	*)
+		cmd_usage "$1";
+		;;
+	esac
+}
+
+# $1 - action; $2 - journal date | pattern string.;
+process_journal() {
+	if [ "$#" -lt 2 ]; then
+		# shortcut to create/open a new journal for today's date
+		create_journal "$(date +"%Y-%m-%d")";
+	fi
+	case "$2" in
+	open)
+		if [ "$#" -lt 3 ]; then cmd_usage "$1"; fi
+		case "$3" in
+		today)
+			create_journal "$(date +"%Y-%m-%d")";
+			;;
+		yesterday)
+			create_journal "$(date --date="yesterday" +"%Y-%m-%d")";
+			;;
+		tomorrow)
+			create_journal "$(date --date="tomorrow" +"%Y-%m-%d")";
+			;;
+		*)
+			if date -d "$3" "+%Y-%m-%d" >/dev/null 2>&1; then
+				create_journal "$3.md";
+			else
+				printf "Usage: fern journal open yesterday|today|tomorrow|<YYYY-MM-DD>";
+			fi
+			;;
+		esac
+		;;
+	find)
+		if [ "$#" -lt 4 ]; then cmd_usage "$1"; fi
+		find_items "$3" "$4" "$fJournals";
+		;;
+	*)
+		cmd_usage "$1";
+		;;
+	esac
+}
+
+# $1 - action; $2 - note name | pattern string; $3 - note name | template name;
+process_note() {
+	if [ "$#" -lt 2 ]; then cmd_usage "$1"; fi
+	case "$2" in
+	open)
+		if [ "$#" -lt 3 ]; then cmd_usage "$1"; fi
+		target=$(file_checks "$3" "Usage: fern note open <note_name>");
+		if [ ! -e "$fNotes/$target" ]; then
+			print_err "Error: Note not found with the name '$target'.";
+		fi
+		$EDITOR "$fNotes/$target";
+		;;
+	add)
+		if [ "$#" -lt 3 ]; then cmd_usage "$1"; fi
+		targetF=$(file_checks "$3" "Usage: fern note add <note_name> [<template_name>]");
+		if [ "$#" -lt 4 ]; then cmd_usage "$1"; fi
+		targetT=$(file_checks "$4");
+		if [ -e "$fNotes/$targetF" ]; then
+			print_err "Error: Note already exists with the name '$targetF'.";
+		fi
+		if [ -n "$targetT" ]; then
+			if [ ! -e "$fTemplates/$targetT" ]; then
+				print_err "Error: Template not found with the name '$targetT'.";
+			fi
+			# create a new Note from a Template file
+			cp "$fTemplates/$targetT" "$fNotes/$targetF";
+		fi
+		$EDITOR "$fNotes/$targetF";
+		;;
+	del)
+		if [ "$#" -lt 3 ]; then cmd_usage "$1"; fi
+		target="$(file_checks "$3" )";
+		if [ -e "$fNotes/$target" ]; then
+			rm -f "$fNotes/$target";
+			update_internal_links "$target" "\[!FILE REMOVED $target\]";
+		else
+			print_err "Error: Note not found with the name '$target'.";
+		fi
+		;;
+	move)
+		if [ "$#" -lt 3 ]; then cmd_usage "$1"; fi
+		targetOld=$(file_checks "$3" "Usage: fern note move <old_note> <new_note>");
+		if [ "$#" -lt 4 ]; then cmd_usage "$1"; fi
+		targetNew=$(file_checks "$4" "Usage: fern note move <old_note> <new_note>");
+		if [ ! -e "$fNotes/$targetOld" ]; then
+			print_err "Error: Note not found with the name '$targetOld'.";
+		fi
+		if [ -e "$fNotes/$targetNew" ]; then
+			print_err "Error: Note already exists with the name '$targetNew'.";
+		fi
+		mv "$fNotes/$targetOld" "$fNotes/$targetNew";
+		update_internal_links "$targetOld" "$targetNew";
+		;;
+	find)
+		if [ "$#" -lt 3 ]; then
+			cmd_usage "$1";
+		elif [ "$#" -eq 3 ]; then
+			find_items "$3" "" "$fNotes";
+		elif [ "$#" -eq 4 ]; then
+			find_items "$3" "$4" "$fNotes";
+		fi
+		;;
+	*)
+		cmd_usage "$1";
+		;;
+	esac
+}
+
+# $1 - filename; [ $2 - usage error message; ]
+file_checks() {
+	# if not defined/empty,
+	if [ -z "$1" ]; then
+		# if usage notice exists, display notice and stop
+		if [ -n "$2" ]; then
+			printf "%s\n" "$2";
+			exit 0;
+		fi
+		# else spit back the arg
+		printf "%s" "$1";
+	# check if ends in '.md' and append if not passed
+	elif [[ "$1" == *.md ]]; then
+		printf "%s" "$1";
+	else
+		printf "%s.md" "$1";
+	fi
+}
+
+# $1 - journal date;
+create_journal() {
+	if [ ! -e "$fJournals/$1.md" ]; then
+		cp "$dj" "$fJournals/$1.md";
+		sed --in-place "s/{{DATE}}/$1/g" "$fJournals/$1.md"
+	fi
+	$EDITOR "$fJournals/$1.md";
+	exit 0;
+}
+
+# $1 - old name; $2 - new name;
+update_internal_links() {
+	# update internal links in all Notes, Journals, Templates and Bookmarks
+	find "$fNotes" -type f -print0 | xargs -0 sed --in-place "s/$1/$2/g"
+	find "$fJournals" -type f -print0 | xargs -0 sed --in-place "s/$1/$2/g"
+	find "$fTemplates" -type f -print0 | xargs -0 sed --in-place "s/$1/$2/g"
+	sed --in-place "s/$1/$2/g" "$fBookmarks"
+}
+
+# $1 - pattern; $2 - verbose flag; $3 - Folder of items to search over;
+find_items() {
+	if [ -n "$2" ] && [ "$2" = "--verbose" ]; then
+		grep  --dereference-recursive --line-number --ignore-case "$1" "$3" | sort | less;
+	else
+		grep --dereference-recursive --line-number --ignore-case --files-with-matches "$1" "$3" | sort;
+	fi
+}
+
+# $1 - Error message to print;
+print_err() {
+	printf "%s\n" "$1" >&2;
+	exit 1;
+}
+
+
+main "${@}";
